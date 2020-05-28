f12218901fe26709f4e53f465d5b83f2dcab1b59
Erica Sanchez <sanchez.ericat@gmail.com>
2020-05-28 12:45:04 -0700

    fixed typo in user class and added pycache in .gitignore

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 class User(db.Model):
     """A user."""
 
-    __tablename __ = 'users'
+    __tablename__ = 'users'
 
     user_id = db.Column(db.Integer,
                         primary_key=True,
